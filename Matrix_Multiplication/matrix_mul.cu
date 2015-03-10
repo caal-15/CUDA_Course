7b7bffbec0e6990c2c2ff766be93f3fa8c89cc86
Carlos <carlos@CaalDesktop.home>
2015-03-10 17:35:39 -0500

    Added tiling, float support

--- Matrix_Multiplication/matrix_mul.cu
+++ Matrix_Multiplication/matrix_mul.cu
@@ -1,12 +1,16 @@
 #include <cuda.h>
 #include <bits/stdc++.h>
 
+#define BLOCK_SIZE 32
+
 using namespace std;
+
+
 
 void fill_matrix_random(int *mat, int rows, int cols){
   for (int i = 0; i < rows; i++){
     for (int j = 0; j < cols; j++){
-      mat[i * cols + j] = rand() % 99;
+      mat[i * cols + j] = rand() % 5;
     }
   }
 }
@@ -56,6 +60,68 @@ __global__ void mat_mul_kernel(int *m_A, int *m_B, int *m_C, int A_rows, int A_c
   }
 }
 
+__global__ void mat_mul_kernel_tiled(int *m_A, int *m_B, int *m_C, int A_rows, int A_cols, int B_rows, int B_cols){
+  __shared__ int s_A[BLOCK_SIZE][BLOCK_SIZE], s_B[BLOCK_SIZE][BLOCK_SIZE];
+
+  int blockRow = blockIdx.y;
+  int blockCol = blockIdx.x;
+
+  int threadRow = threadIdx.y;
+  int threadCol = threadIdx.x;
+
+  int row = blockRow * BLOCK_SIZE + threadRow;
+  int col = blockCol * BLOCK_SIZE + threadCol;
+
+  int sum = 0;
+  for (int sm = 0; sm < A_cols / BLOCK_SIZE; sm++){
+    if (row < A_rows && (sm * BLOCK_SIZE + threadCol) < A_cols){
+      s_A[threadRow][threadCol] = m_A[(row) * A_cols + (sm * BLOCK_SIZE + threadCol)];
+    } else{
+      s_A[threadRow][threadCol] = 0;
+    }
+    if (col < B_cols && (threadRow + sm * BLOCK_SIZE) < B_rows){
+      s_B[threadRow][threadCol] = m_B[(threadRow + sm * BLOCK_SIZE) * B_cols + (col)];
+    } else{
+      s_B[threadRow][threadCol] = 0;
+    }
+    __syncthreads();
+    for (int i = 0; i < BLOCK_SIZE; i++){
+      sum += s_A[threadRow][i] * s_B[i][threadCol];
+    }
+    __syncthreads();
+    if (row < A_rows && col < B_cols)
+      m_C[row * B_cols + col] = sum;
+  }
+
+}
+
+/*__global__ void mat_mul_kernel_tiled (int *A, int *B, int *C, int ARows, int ACols, int BRows, int BCols){
+  int CValue = 0;
+
+  int Row = blockIdx.y*BLOCK_SIZE + threadIdx.y;
+  int Col = blockIdx.x*BLOCK_SIZE + threadIdx.x;
+
+  __shared__ int As[BLOCK_SIZE][BLOCK_SIZE];
+  __shared__ int Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+  for (int k = 0; k < (BLOCK_SIZE + ACols - 1)/BLOCK_SIZE; k++) {
+
+       if (k*BLOCK_SIZE + threadIdx.x < ACols && Row < ARows)   As[threadIdx.y][threadIdx.x] = A[Row*ACols + k*BLOCK_SIZE + threadIdx.x];
+       else                                                   As[threadIdx.y][threadIdx.x] = 0;
+
+       if (k*BLOCK_SIZE + threadIdx.y < BRows && Col < BCols)   Bs[threadIdx.y][threadIdx.x] = B[(k*BLOCK_SIZE + threadIdx.y)*BCols + Col];
+       else                                                   Bs[threadIdx.y][threadIdx.x] = 0;
+
+       __syncthreads();
+
+       for (int n = 0; n < BLOCK_SIZE; ++n) CValue += As[threadIdx.y][n] * Bs[n][threadIdx.x];
+
+       __syncthreads();
+    }
+
+    if (Row < ARows && Col < BCols) C[((blockIdx.y * blockDim.y + threadIdx.y)*BCols)+(blockIdx.x*blockDim.x)+threadIdx.x]=CValue;
+}*/
+
 void mat_mul_con(int *m_A, int *m_B, int *m_C, int A_rows, int A_cols, int B_rows, int B_cols){
     int A_size = A_rows * A_cols * sizeof(int);
     int B_size = B_rows * B_cols * sizeof(int);
@@ -70,9 +136,34 @@ void mat_mul_con(int *m_A, int *m_B, int *m_C, int A_rows, int A_cols, int B_row
     cudaMemcpy(d_A, m_A, A_size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, m_B, B_size, cudaMemcpyHostToDevice);
     //3. Kernel Launch Code
-    dim3 dimGrid(ceil(max(A_rows, B_rows)/32.0), ceil(max(A_cols, B_cols)/32.0), 1);
-    dim3 dimBlock(32, 32, 1);
+    dim3 dimGrid(ceil(max(A_rows, B_rows)/BLOCK_SIZE), ceil(max(A_cols, B_cols)/BLOCK_SIZE), 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
     mat_mul_kernel<<<dimGrid, dimBlock>>> (d_A, d_B, d_C, A_rows, A_cols, B_rows, B_cols);
+    cudaDeviceSynchronize();
+    //4. Copy d_C to C from device, free device memory (cusdaFree), sync if neccessary
+    cudaMemcpy (m_C, d_C, C_size, cudaMemcpyDeviceToHost);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+}
+
+void mat_mul_con_tiled(int *m_A, int *m_B, int *m_C, int A_rows, int A_cols, int B_rows, int B_cols){
+    int A_size = A_rows * A_cols * sizeof(int);
+    int B_size = B_rows * B_cols * sizeof(int);
+    int C_size = A_rows * B_cols * sizeof(int);
+
+    int *d_A, *d_B, *d_C;
+    //1. Allocate memory for d_A, etc. on the device (cudaMalloc)
+    cudaMalloc(&d_A, A_size);
+    cudaMalloc(&d_B, B_size);
+    cudaMalloc(&d_C, C_size);
+    //2. Copy Data from host to d_A, etc. (cudaMemcpy)
+    cudaMemcpy(d_A, m_A, A_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, m_B, B_size, cudaMemcpyHostToDevice);
+    //3. Kernel Launch Code
+    dim3 dimGrid(ceil(max(A_rows, B_rows)/BLOCK_SIZE), ceil(max(A_cols, B_cols)/BLOCK_SIZE), 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+    mat_mul_kernel_tiled<<<dimGrid, dimBlock>>> (d_A, d_B, d_C, A_rows, A_cols, B_rows, B_cols);
     cudaDeviceSynchronize();
     //4. Copy d_C to C from device, free device memory (cusdaFree), sync if neccessary
     cudaMemcpy (m_C, d_C, C_size, cudaMemcpyDeviceToHost);
@@ -94,12 +185,13 @@ int main(int argc, char **argv){
 
   ofstream x("x.mio"),
   y_seq("y_seq.mio"),
-  y_con("y_con.mio");
+  y_con("y_con.mio"),
+  y_con_tiled("y_con_tiled.mio");
 
   clock_t begin, end;
   double elapsed_secs;
 
-  for (int i = step; i < max_number; i += step){
+  for (int i = step; i <= max_number; i += step){
     int *A, *B, *C, *D;
     A = (int*) malloc((i + offset_A) * i * sizeof(int));
     B = (int*) malloc((i + offset_B) * i * sizeof(int));
@@ -123,10 +215,28 @@ int main(int argc, char **argv){
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     y_con << elapsed_secs << endl;
 
+    cout << "-----------" << endl << "Not Tiled: ";
     if (check_matrix(C, D, i + offset_A, i + offset_B))
       cout << "All good" << endl;
     else
       cout << "Something Went Wrong" << endl;
+    cout << "-----------" << endl;
+
+    begin = clock();
+    mat_mul_con_tiled(A, B, D, i + offset_A, i, i, i + offset_B);
+    end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    y_con_tiled << elapsed_secs << endl;
+    /*print_matrix(A, i + offset_A, i);
+    print_matrix(B, i, i + offset_B);
+    print_matrix(C, i + offset_A, i + offset_B);
+    print_matrix(D, i + offset_A, i + offset_B);*/
+    cout << "Tiled: ";
+    if (check_matrix(C, D, i + offset_A, i + offset_B))
+      cout << "All good" << endl;
+    else
+      cout << "Something Went Wrong" << endl;
+    cout << "-----------" << endl;
 
     free(A);
     free(B);
