9ced86a20b11415dcb8b556b0440fc8e52d6b94a
caal-15 <caal.0522@gmail.com>
2015-04-20 13:30:58 -0500

    Added working code

---
+++ Convolution_2D/build/CMakeFiles/convolution2d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convolution2d.dir/./convolution2d_generated_convolution_2d.cu.o"
+  "convolution2d.pdb"
+  "convolution2d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang)
+  include(CMakeFiles/convolution2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Convolution_2D/convolution_2d.cu
+++ Convolution_2D/convolution_2d.cu
@@ -5,12 +5,22 @@
 #define BLOCK_SIZE 32
 #define EPSILON 0.1
 #define MASK_SIZE 3
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 
 using namespace std;
 using namespace cv;
 
 __constant__ char SOBEL[MASK_SIZE * MASK_SIZE];
 
+
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
 
 __device__ unsigned char clamp(int value){
   if(value < 0)
@@ -21,86 +31,7 @@ __device__ unsigned char clamp(int value){
   return (unsigned char)value;
 }
 
-__global__ void convolution_kernel_tiled(unsigned char *data, unsigned char *result, int width, int height){
-  // Data cache: threadIdx.x , threadIdx.y
-  int n = MASK_SIZE / 2;
-  __shared__ int s_data[BLOCK_SIZE + MASK_SIZE - 1][BLOCK_SIZE + MASK_SIZE - 1];
 
-  // global mem address of this thread
-  const int pos = threadIdx.x + (blockIdx.x * blockDim.x) + (threadIdx.y * width) + (blockIdx.y * blockDim.y) * width;
-
-  // load cache (32x32 shared memory, 16x16 threads blocks)
-  // each threads loads four values from global memory into shared mem
-  // if in image area, get value in global mem, else 0
-  int x, y; // image based coordinate
-
-  // original image based coordinate
-  const int x0 = threadIdx.x + (blockIdx.x * blockDim.x);
-  const int y0 = threadIdx.y + (blockIdx.y * blockDim.y);
-
-  // case1: upper left
-  x = x0 - n;
-  y = y0 - n;
-  if ( x < 0 || y < 0 )
-    s_data[threadIdx.x][threadIdx.y] = 0;
-  else
-    s_data[threadIdx.x][threadIdx.y] = data[ pos - n - (width * n)];
-
-  // case2: upper right
-  x = x0 + n;
-  y = y0 - n;
-  if ( x > width - 1 || y < 0 )
-    s_data[threadIdx.x + blockDim.x][threadIdx.y] = 0;
-  else
-    s_data[threadIdx.x + blockDim.x][threadIdx.y] = data[pos + n - (width * n)];
-
-  // case3: lower left
-  x = x0 - n;
-  y = y0 + n;
-  if (x < 0 || y > height - 1)
-    s_data[threadIdx.x][threadIdx.y + blockDim.y] = 0;
-  else
-    s_data[threadIdx.x][threadIdx.y + blockDim.y] = data[pos - n + (width * n)];
-
-  // case4: lower right
-  x = x0 + n;
-  y = y0 + n;
-  if ( x > width - 1 || y > height - 1)
-    s_data[threadIdx.x + blockDim.x][threadIdx.y + blockDim.y] = 0;
-  else
-    s_data[threadIdx.x + blockDim.x][threadIdx.y + blockDim.y] = data[pos + n + (width * n)];
-
-  __syncthreads();
-
-  // convolution
-  int sum = 0;
-  x = n + threadIdx.x;
-  y = n + threadIdx.y;
-  for (int i = - n; i <= n; i++)
-    for (int j = - n; j <= n; j++)
-      sum += s_data[x + i][y + j] * SOBEL[n + j] * SOBEL[n + i];
-
-  result[pos] = clamp(sum);
-}
-
-void convolution_con_tiled (unsigned char *data, unsigned char *result, int width, int height){
-  unsigned char *d_data, *d_result;
-
-  cudaMalloc(&d_data, sizeof(unsigned char) * width * height);
-  cudaMalloc(&d_result, sizeof(unsigned char) * width * height);
-
-  cudaMemcpy(d_data, data, sizeof(unsigned char) * width * height, cudaMemcpyHostToDevice);
-
-  dim3 dimGrid (ceil (width / float (BLOCK_SIZE)), ceil (height / float (BLOCK_SIZE)), 1);
-  dim3 dimBlock (BLOCK_SIZE, BLOCK_SIZE, 1);
-
-  convolution_kernel_tiled<<<dimGrid, dimBlock>>> (d_data, d_result, width, height);
-
-  cudaMemcpy(result, d_result, sizeof(unsigned char) * width * height, cudaMemcpyDeviceToHost);
-
-  cudaFree(d_data);
-  cudaFree(d_result);
-}
 
 __global__ void img2gray_kernel(unsigned char *data, unsigned char *result, int width, int height){
   int row = blockIdx.y*blockDim.y+threadIdx.y;
@@ -123,6 +54,70 @@ void img2gray(unsigned char *data, unsigned char *result, int width, int height)
   dim3 dimBlock (BLOCK_SIZE, BLOCK_SIZE, 1);
 
   img2gray_kernel <<<dimGrid, dimBlock>>> (d_data, d_result, width, height);
+  gpuErrchk( cudaPeekAtLastError() );
+  gpuErrchk( cudaDeviceSynchronize() );
+  cudaMemcpy(result, d_result, sizeof(unsigned char) * width * height, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_data);
+  cudaFree(d_result);
+}
+
+__global__ void convolution_kernel_tiled (unsigned char *data, unsigned char *result, int width, int height) {
+   __shared__ int s_data[BLOCK_SIZE + MASK_SIZE - 1][BLOCK_SIZE + MASK_SIZE - 1];
+  // First batch loading
+  const int radius = MASK_SIZE / 2;
+  int dest = threadIdx.y * BLOCK_SIZE + threadIdx.x,
+     destY = dest / (BLOCK_SIZE + MASK_SIZE - 1), destX = dest % (BLOCK_SIZE + MASK_SIZE - 1),
+     srcY = blockIdx.y * BLOCK_SIZE + destY - radius,
+     srcX = blockIdx.x * BLOCK_SIZE + destX - radius,
+     src = srcY * width + srcX;
+  if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+     s_data[destY][destX] = data[src];
+  else
+     s_data[destY][destX] = 0;
+
+  // Second batch loading
+  dest = threadIdx.y * BLOCK_SIZE + threadIdx.x + BLOCK_SIZE * BLOCK_SIZE;
+  destY = dest / (BLOCK_SIZE + MASK_SIZE - 1), destX = dest % (BLOCK_SIZE + MASK_SIZE - 1);
+  srcY = blockIdx.y * BLOCK_SIZE + destY - radius;
+  srcX = blockIdx.x * BLOCK_SIZE + destX - radius;
+  src = srcY * width + srcX;
+  if (destY < BLOCK_SIZE + MASK_SIZE - 1) {
+     if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+        s_data[destY][destX] = data[src];
+     else
+        s_data[destY][destX] = 0;
+  }
+
+  __syncthreads();
+
+  int sum = 0;
+  int y, x;
+  for (y = 0; y < MASK_SIZE; y++)
+     for (x = 0; x < MASK_SIZE; x++)
+        sum += s_data[threadIdx.y + y][threadIdx.x + x] * SOBEL[y * MASK_SIZE + x];
+  y = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+  x = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+  if (y < height && x < width)
+    result[y * width + x] = clamp(sum);
+  __syncthreads();
+}
+
+void convolution_con_tiled (unsigned char *data, unsigned char *result, int width, int height){
+  unsigned char *d_data, *d_result;
+
+  cudaMalloc (&d_data, sizeof(unsigned char) * width * height);
+  cudaMalloc (&d_result, sizeof(unsigned char) * width * height);
+
+  cudaMemcpy (d_data, data, sizeof(unsigned char) * width * height, cudaMemcpyHostToDevice);
+
+  dim3 dimGrid (ceil (width / float (BLOCK_SIZE)), ceil (height / float (BLOCK_SIZE)), 1);
+  dim3 dimBlock (BLOCK_SIZE, BLOCK_SIZE, 1);
+
+  convolution_kernel_tiled<<<dimGrid, dimBlock>>> (d_data, d_result, width, height);
+  gpuErrchk( cudaPeekAtLastError() );
+  gpuErrchk( cudaDeviceSynchronize() );
+
   cudaMemcpy(result, d_result, sizeof(unsigned char) * width * height, cudaMemcpyDeviceToHost);
 
   cudaFree(d_data);
@@ -135,30 +130,25 @@ int main (int argc, char **argv){
   }
 
   char mask[] = {-1,0,1,-2,0,2,-1,0,1};
-  cudaMemcpyToSymbol (SOBEL, mask, 3 * 3 * sizeof (char));
+  gpuErrchk(cudaMemcpyToSymbol (SOBEL, mask, 3 * 3 * sizeof (char)));
 
   Mat image, result;
-  image = imread("../img1.jpg",1);
-  //imwrite ("miau.jpg", image);
+  image = imread("../img1.jpg",0);
 
   Size s = image.size();
   int Row = s.width;
   int Col = s.height;
 
-  unsigned char * In = (unsigned char*)malloc (sizeof (unsigned char)* Row * Col * image.channels());
   unsigned char *Gray = (unsigned char*)malloc (sizeof (unsigned char)* Row * Col);
-  unsigned char * Out = (unsigned char*)malloc (sizeof (unsigned char)* Row * Col);
+  unsigned char *Out = (unsigned char*)malloc (sizeof (unsigned char)* Row * Col);
 
-  In = image.data;
+  Gray = image.data;
 
-  img2gray (In, Gray, Row, Col);
+  convolution_con_tiled(Gray, Out, Row, Col);
   result.create(Row, Col, CV_8UC1);
   result.data = Out;
   imwrite ("miau.jpg", result);
 
-
-
-  //convolution_con_tiled (Gray, Out, Row, Col);
 
 
 
